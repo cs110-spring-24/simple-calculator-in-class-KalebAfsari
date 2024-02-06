fd4f8ec5fe439af6727a3969d365ed83bc0411ca
KalebAfsari <blueskaleb12@gmail.com>
2024-02-05 16:10:28 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,31 @@
+num1 = int(input("Give me a number: "))
+operation = input("Select an operation: ")
+num2 = int(input("Give me another number: "))
 
+if operation == "+":
+	total  = num1 + num2
+	print (f"{num1} + {num2} = {total}")
+
+elif operation == "-":
+	total = num1 - num2
+	print (f"{num1} - {num2} = {total}")
+
+elif operation == "*": 
+	total = num1 * num2
+	print (f"{num1} * {num2} = {total}")
+
+elif operation == "/":
+	total = num1 / num2
+	print (f"{num1} / {num2} = {total}")
+
+elif operation == "**":
+	total = num1 ** num2
+	print (f"{num1} ** {num2} = {total}")
+
+elif operation == "//":
+	total = num1 // num2
+	print (f"{num1} // {num2} = {total}")
+
+elif operation == "%":
+	total = num1 % num2
+	print (f"{num1} % {num2} = {total}")
